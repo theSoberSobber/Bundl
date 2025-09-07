54007931c3521764c608214603e48c5539b91e12
theSoberSobber <pavitchhabra1611@gmail.com>
2025-09-08 02:03:03 +0530

    upgrade to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 
 android {
     namespace = "com.pavit.bundl"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.pavit.bundl"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
