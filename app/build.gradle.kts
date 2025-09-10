d94810725fad917d6845bf27015c7e7761ad8788
theSoberSobber <pavitchhabra1611@gmail.com>
2025-09-10 15:21:43 +0530

    upgrade versionCode for play console

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.pavit.bundl"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
