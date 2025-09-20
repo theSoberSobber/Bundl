9ecb6dd85025fdfc2488b78e08cadfc0122c4e19
theSoberSobber <pavitchhabra1611@gmail.com>
2025-09-21 00:18:39 +0530

    swap hostnames from local to remote

--- app/src/main/java/com/pavit/bundl/utils/network/NetworkConfig.kt
+++ app/src/main/java/com/pavit/bundl/utils/network/NetworkConfig.kt
@@ -1,14 +1,14 @@
 package com.pavit.bundl.utils.network
 
 object NetworkConfig {
-    // private const val BASE_HOSTNAME = "backend-bundl.1110777.xyz"
+    private const val BASE_HOSTNAME = "backend-bundl.1110777.xyz"
     // private const val BASE_HOSTNAME = "192.168.53.152:3002" // Local dev
-    private const val BASE_HOSTNAME = "10.0.2.2:3002" // Local dev
+    // private const val BASE_HOSTNAME = "10.0.2.2:3002" // Local dev
     
-    // const val BASE_URL = "https://$BASE_HOSTNAME"
-    // const val BASE_WS_URL = "wss://$BASE_HOSTNAME/chat"
+    const val BASE_URL = "https://$BASE_HOSTNAME"
+    const val BASE_WS_URL = "wss://$BASE_HOSTNAME/chat"
     
     // For local development, uncomment these:
-    const val BASE_URL = "http://$BASE_HOSTNAME"
-    const val BASE_WS_URL = "ws://$BASE_HOSTNAME/chat"
+    // const val BASE_URL = "http://$BASE_HOSTNAME"
+    // const val BASE_WS_URL = "ws://$BASE_HOSTNAME/chat"
 }
