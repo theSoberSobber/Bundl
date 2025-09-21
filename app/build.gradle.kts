f4bacc855fbacf5f0078fbdfc0f6ef720a2532bb
theSoberSobber <pavitchhabra1611@gmail.com>
2025-09-21 19:54:06 +0530

    version code update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.pavit.bundl"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.0"
+        versionCode = 6
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
