b1cd0490bc4f52b68dcde5074e41f5468c8858d6
theSoberSobber <pavitchhabra1611@gmail.com>
2025-09-22 10:51:55 +0530

    bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.pavit.bundl"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.1.2"
+        versionCode = 8
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
